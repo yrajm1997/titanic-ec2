064b8cbfa01badc87ac61de83fc2c99793311588
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2024-07-19 15:27:02 +0530

    added tracking uri in train file

--- titanic_model/train_pipeline.py
+++ titanic_model/train_pipeline.py
@@ -19,21 +19,6 @@ def run_training() -> None:
     """
     Train the model.
     """
-
-    import mlflow
-    # Set an experiment name, unique and case-sensitive
-    # It will create a new experiment if the experiment with given doesn't exist
-    exp = mlflow.set_experiment(experiment_name = "Titanic-Survival-Pred")
-
-    # Start RUN
-    mlflow.start_run(experiment_id= exp.experiment_id)        # experiment id under which to create the current run
-    
-    # Log parameters
-    mlflow.log_param("n_estimators", config.model_config.n_estimators)
-    mlflow.log_param("max_depth", config.model_config.max_depth)
-    mlflow.log_param("max_features", config.model_config.max_features)
-    mlflow.log_param("random_state", config.model_config.random_state)
-
     
     # read training data
     data = load_dataset(file_name=config.app_config.training_data_file)
@@ -109,11 +94,30 @@ def run_training() -> None:
         mlflow.sklearn.log_model(sk_model = titanic_pipe, 
                                 artifact_path= "trained_model")
 
-
-    # End an active MLflow run
-    mlflow.end_run()
     
 
 if __name__ == "__main__":
+    print("Re-training:", os.environ['RE_TRAIN'])
     if os.environ['RE_TRAIN']:
+        
+        import mlflow
+        # Set the tracking URI to the server
+        mlflow.set_tracking_uri("http://192.168.43.86:5000")
+        # Set an experiment name, unique and case-sensitive
+        # It will create a new experiment if the experiment with given doesn't exist
+        exp = mlflow.set_experiment(experiment_name = "Titanic-Survival-Pred")
+
+        # Start RUN
+        mlflow.start_run(experiment_id= exp.experiment_id)        # experiment id under which to create the current run
+        
+        # Log parameters
+        mlflow.log_param("n_estimators", config.model_config.n_estimators)
+        mlflow.log_param("max_depth", config.model_config.max_depth)
+        mlflow.log_param("max_features", config.model_config.max_features)
+        mlflow.log_param("random_state", config.model_config.random_state)
+        
         run_training()
+        
+        # End an active MLflow run
+        mlflow.end_run()
+        
